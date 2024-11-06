bd4719ef29056b49bf8d015841a7970790e7c24b
Ray-Hossain <rxh220035@utdallas.edu>
2024-11-05 18:22:01 -0600

    Implemented random placement of watermark.

--- src/watermark.py
+++ src/watermark.py
@@ -1,9 +1,12 @@
 from PIL import Image
+import random
 
 def main():
     with (Image.open('img//sample_img.png') as img,
           Image.open('watermark(s)//sample_watermark.png') as wm):
-        img.paste(wm, (500, 500), mask=wm.getchannel('A'))
+        x = random.randrange(10, (img.width-wm.width))
+        y = random.randrange(10, (img.height-wm.height))
+        img.paste(wm, (x, y), mask=wm.getchannel('A'))
         img.show()
 
 if __name__ == "__main__":
