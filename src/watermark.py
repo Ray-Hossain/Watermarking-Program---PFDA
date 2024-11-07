8fd3528bcbef65b734111f134c88f69aef7408ad
Ray-Hossain <rxh220035@utdallas.edu>
2024-11-07 10:45:49 -0600

    Added new images to test and implemented resizing and multiply function. Need to work out bugs.

--- src/watermark.py
+++ src/watermark.py
@@ -1,12 +1,14 @@
-from PIL import Image
+from PIL import Image, ImageChops
 import random
 
 def main():
-    with (Image.open('img//sample_img.png') as img,
-          Image.open('watermark(s)//sample_watermark.png') as wm):
-        x = random.randrange(10, (img.width-wm.width))
-        y = random.randrange(10, (img.height-wm.height))
-        img.paste(wm, (x, y), mask=wm.getchannel('A'))
+    with (Image.open('img//RetroComputer_IJustGoByRay.png') as img,
+          Image.open('watermark(s)//IJustGoByRay_LOGO_C.png') as wm):
+        resized_wm = wm.resize((wm.width//2, wm.height//2))
+        multiply_wm = ImageChops.multiply(img, resized_wm)
+        x = random.randrange(10, (img.width-resized_wm.width))
+        y = random.randrange(10, (img.height-resized_wm.height))
+        img.paste(multiply_wm, (x, y), mask=multiply_wm.getchannel('A'))
         img.show()
 
 if __name__ == "__main__":
