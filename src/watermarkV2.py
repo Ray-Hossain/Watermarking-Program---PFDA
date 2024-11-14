b2ac42263bd7558a484c8f65c9d834c2fbd6b0a8
Ray-Hossain <rxh220035@utdallas.edu>
2024-11-14 17:17:41 -0600

    Added tkinter library to open file explorer for the WaterMarker program. Save function still in progress.

--- src/watermarkV2.py
+++ src/watermarkV2.py
@@ -1,5 +1,7 @@
 import pygame
-import os
+from tkinter import filedialog
+from PIL import Image
+import random
 
 class Shape():
     def __init__(self, surface, color, pos, size, function):
@@ -50,13 +52,37 @@ class UI():
                 if (shape.pos[0] < pos[0] and (shape.pos[0] + shape.size[0]) > pos[0] and 
                     shape.pos[1] < pos[1] and (shape.pos[1] + shape.size[1]) > pos[1]):
                     return shape.function
-            return None
 
 def gui_init(art):
-    art.define_shape(pos=(100, 100), size=(50, 50), color=(255, 0, 0), function="test")
+    # for opening image button
+    art.define_shape(pos=(100, 100), size=(100, 50), color=(255, 0, 0), function="open_img")
+    art.define_text(pos=(100, 100), size=50, text="IMG")
+    # for opening watermark button
+    art.define_shape(pos=(300, 100), size=(100, 50), color=(255, 0, 0), function="open_wm")
+    art.define_text(pos=(300, 100), size=50, text="WM")
+    # for saving image button
+    art.define_shape(pos=(300, 300), size=(100, 50), color=(255, 0, 0), function="save")
+    art.define_text(pos=(300, 300), size=50, text="Save")
 
-def test(press):
-    print("pressed")
+def open_img(press):
+    file_path = filedialog.askopenfilename()
+    return file_path
+
+def open_wm(press):
+    file_path = filedialog.askopenfilename()
+    return file_path
+
+def save(image, watermark):
+    filepath = filedialog.askdirectory()
+    with (Image.open(image) as img,
+          Image.open(watermark) as wm):
+        resized_wm = wm.resize((wm.width//2, wm.height//2))
+        new_img = Image.new('RGBA', size=(img.width, img.height), color=(255, 255, 255, 0))
+        x = random.randrange(5, (new_img.width-resized_wm.width))
+        y = random.randrange(5, (new_img.height-resized_wm.height))
+        new_img.paste(resized_wm, (x, y), mask=resized_wm.getchannel('A'))
+        img.paste(new_img, (0,0), mask=new_img.getchannel('A'))
+        img.save(filepath+"/test.png")
 
 def main():
     pygame.init()
@@ -70,8 +96,12 @@ def main():
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 function = art.button(pygame.mouse.get_pos())
-                if function != None:
-                    exec(f"{function}" + "(None)")
+                if function == "open_img":
+                    img = exec(f"{function}" + "(None)")
+                if function == "open_wm":
+                    wm = exec(f"{function}" + "(None)")
+                if function == "save":
+                    exec(f"{function}({img},{wm})")
         screen.fill(pygame.Color(30, 30, 30, 255))
         art = UI(screen)
         gui_init(art)
