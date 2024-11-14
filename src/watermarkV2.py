113d72a0ab71147a61b4cb065e39bfa3fd48b2ec
Ray-Hossain <rxh220035@utdallas.edu>
2024-11-14 16:24:45 -0600

    Added button method inside UI class.

--- src/watermarkV2.py
+++ src/watermarkV2.py
@@ -1,11 +1,13 @@
 import pygame
+import os
 
 class Shape():
-    def __init__(self, surface, color, pos, size):
+    def __init__(self, surface, color, pos, size, function):
         self.surface = surface
         self.pos = pos
         self.size = size
         self.color = pygame.Color(color)
+        self.function = function
 
     def draw(self):
         pygame.draw.rect(self.surface, self.color, ((self.pos), (self.size)))
@@ -28,8 +30,8 @@ class UI():
         self.shape = []
         self.text = []
 
-    def define_shape(self, pos, size, color):
-        shape = Shape(surface=self.surface, pos=pos, size=size, color=color)
+    def define_shape(self, pos, size, color, function):
+        shape = Shape(surface=self.surface, pos=pos, size=size, color=color, function=function)
         self.shape.append(shape)  
 
     def define_text(self, text, pos, size):
@@ -42,8 +44,19 @@ class UI():
         for text in self.text:
             text.draw()
 
+    def button(self, pos):
+        for shape in self.shape:
+            if shape.function != None:
+                if (shape.pos[0] < pos[0] and (shape.pos[0] + shape.size[0]) > pos[0] and 
+                    shape.pos[1] < pos[1] and (shape.pos[1] + shape.size[1]) > pos[1]):
+                    return shape.function
+            return None
+
 def gui_init(art):
-    return
+    art.define_shape(pos=(100, 100), size=(50, 50), color=(255, 0, 0), function="test")
+
+def test(press):
+    print("pressed")
 
 def main():
     pygame.init()
@@ -55,6 +68,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                function = art.button(pygame.mouse.get_pos())
+                if function != None:
+                    exec(f"{function}" + "(None)")
         screen.fill(pygame.Color(30, 30, 30, 255))
         art = UI(screen)
         gui_init(art)
